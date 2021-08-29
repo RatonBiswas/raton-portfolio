5f7439a86293b8b3b1ae65b561c13f5b1ebb4d07
Raton Biswas <32263412+RatonBiswas@users.noreply.github.com>
2021-08-29 12:16:09 +0600

    update resume link

--- src/Components/NavBar/Navbar.js
+++ src/Components/NavBar/Navbar.js
@@ -10,7 +10,7 @@ function Navbar() {
                 <li><NavLink className={classes.navLink} to='/projects' activeClassName={classes.activeLink}>Projects</NavLink></li>
                 <li><NavLink className={classes.navLink} to='/experience' activeClassName={classes.activeLink}>Experience</NavLink></li>
                 <li><NavLink className={classes.navLink} to='/Skills' activeClassName={classes.activeLink}>Skills</NavLink></li>
-                <li><a className={classes.navLink}  href='https://docs.google.com/document/d/1sH4cKinQV8fXoZwh7pzSaqgX1HjjjIRlvBUj3ziMsAo/edit?usp=sharing' target='blank' activeClassName={classes.activeLink}>Resume</a></li>
+                <li><a className={classes.navLink}  href='https://drive.google.com/file/d/16JD6mP4qepdvbZGQGNK5Ki721eQt9m2_/view?usp=sharing' target='blank' activeClassName={classes.activeLink}>Resume</a></li>
             </ul>
         </div>
     )
